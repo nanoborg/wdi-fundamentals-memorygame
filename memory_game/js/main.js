17782bf90719cb531f74308fe5526d3248dd4031
tim <tborg84@gmail.com>
2024-07-09 22:01:25 +1000

    changes made to logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,83 @@
-debugger
+
+console.log("Up and running!");
+var cards = [
+   {
+       rank: "queen",
+       suit: "hearts",
+       cardImage: "images/queen-of-hearts.png",
+   },
+   {
+      rank: "queen",
+      suit: "diamonds",
+      cardImage: "images/queen-of-diamonds.png",
+   },
+
+   {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png",
+   },
+    {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+   },
+];
+
+var cardsInPlay = [];
+
+ function checkForMatch() {
+  //if (cardsInPlay.length == 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+          alert("You found a match!");
+          //setTimeout('location', 50000);
+        //location.reload()
+
+      } else {
+          alert("Sorry, try again");
+        };
+
+  };
 
 
-const cards = ["queen", "queen", "king", "king"];
-const cardsInPlay = [];
+function flipCard() {
+    var cardId = this.getAttribute('data-id');
+    console.log(cardId)
+ //console.log("User flipped" + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+ this.setAttribute('src', cards[cardId].cardImage);
+//console.log(cards[cardId].suit);
+ if (cardsInPlay.length === 2){
+    checkForMatch();
+    //cardsInPlay = [];
 
-function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
-	  } else if (cardsInPlay.length === 2) {
-		console.log('sorry try again');
-	  }
-}
+ };
+};
 
-function flipCard(cardId) {
-	console.log('User flipped '+cards[cardId]);
-	cardsInPlay.push(cardId);
-	checkForMatch();
-}
+console.log(cardsInPlay);
 
-flipCard(0)
-flipCard(2)
+
+ function createBoard() {
+  const cardTable = document.getElementById('game-board');
+  for (var i = 0; i < cards.length; i++) {
+  var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    cardTable.appendChild(cardElement);
+
+  }
+};
+
+createBoard();
+
+
+
+
+
+
+
+
+
+
+
